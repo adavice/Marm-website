a4cf12397659e340b4de81291201c985111f7a39
adavice <97439259+adavice@users.noreply.github.com>
2025-04-14 15:30:00 +0200

    fixed contact form

--- js/form.js
+++ js/form.js
@@ -1,5 +1,5 @@
 
-$("#form").on("submit", function () {
+$("#form2").on("submit", function () {
   var formData = {
     name: $("#name").val(),
     email: $("#email").val(),
@@ -7,7 +7,7 @@ $("#form").on("submit", function () {
     company: $("#company").val(),
     message: $("#message").val(),
   };
-  $("#form").prop("disabled", true);
+  $("#form2").prop("disabled", true);
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({ event: "send_form_talk_to_us" });
 
@@ -18,22 +18,22 @@ $("#form").on("submit", function () {
     encode: true,
     success: function (data) {
       const containerTop = $('.section').offset().top;
-      $('#form').hide();
+      $('#form2').hide();
       $('#successMessage').show().css('display', 'flex');
       
       // Trigger scroll and fade-in animation
       $('html, body').animate({
           scrollTop: containerTop
       }, 100, function() {
-          $('#form').addClass('fade-in');
+          $('#form2').addClass('fade-in');
       });
 
-      $("#form")[0].reset();
-      $("#form").prop("disabled", false);
+      $("#form2")[0].reset();
+      $("#form2").prop("disabled", false);
     },
     error: function (XMLHttpRequest, textStatus, errorThrown) {
       alert("Something is wrong");
-      $("#form").prop("disabled", false);
+      $("#form2").prop("disabled", false);
     },
   });
   return false;
@@ -62,16 +62,16 @@ $(".form-group input, .form-group textarea").each(function () {
 
 /*$(document).ready(function () {
 
-  $("#form").on("submit", function (e) {
+  $("#form2").on("submit", function (e) {
     e.preventDefault();
     const containerTop = $('.section').offset().top;
-      $('#form').hide();
+      $('#form2').hide();
       $('#successMessage').show().css('display', 'flex');
       
       $('html, body').animate({
           scrollTop: containerTop
       }, 100, function() {
-          $('#form').addClass('fade-in');
+          $('#form2').addClass('fade-in');
       });
   });
 
